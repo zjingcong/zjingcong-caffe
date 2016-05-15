7947f32be97aad36bf7fa3c8673206303ce54d7c
zjingcong <jingcong.zhang@outlook.com>
2016-05-15 17:07:56 +0800

    Modify video classifier.

--- zjingcong-caffe/examples/LRCN_activity_recognition/run_lstm_RGB.sh
+++ zjingcong-caffe/examples/LRCN_activity_recognition/run_lstm_RGB.sh
@@ -5,5 +5,9 @@ TOOLS=/home/zjc/workspace/git/zjingcong-caffe/zjingcong-caffe/build/tools
 export HDF5_DISABLE_VERSION_CHECK=1
 export PYTHONPATH=.
 
-GLOG_logtostderr=1  $TOOLS/caffe train -solver lstm_solver_RGB.prototxt -weights /disk/zjingcong/caffemodel/caffe_imagenet_hyb2_wr_rc_solver_sqrt_iter_310000
+LOG=/home/zjc/log/run_lstm_RGB_log/train-$(date +%Y-%m-%d-%M:%m).log
+
+echo "LOG PATH: "
+echo $LOG
+GLOG_logtostderr=1  $TOOLS/caffe train -solver lstm_solver_RGB.prototxt -weights /disk/zjingcong/caffemodel/caffe_imagenet_hyb2_wr_rc_solver_sqrt_iter_310000 > $LOG 2>&1
 echo "Done."
